c6e7e06a84adec8d85bf6a19d75c7cc4132a28d3
Sébastien Le Maire <sebastien.le-maire@epitech.eu>
2021-01-30 14:48:35 +0100

    Refont signUp page

--- srcs/components/forms/SignUpForm/index.jsx
+++ srcs/components/forms/SignUpForm/index.jsx
@@ -1,68 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { KeyboardAvoidingView, Text, TextInput, Button } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
 
-import { validate } from 'validate.js';
-import { IntlConsumer } from '../../../providers/IntlProvider';
+import { Text } from 'react-native-paper';
+
+import { FormInput, FormButton } from '../utils';
+import { IntlContext } from '../../../providers/IntlProvider';
+
+import Styles from './Styles';
 
 const SignUpForm = ({ onSubmit }) => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [errors, setErrors] = React.useState({});
 
+  const intl = React.useContext(IntlContext);
+
+  const constraints = {
+    email: {
+      presence: {
+        allowEmpty: false,
+        message: intl.t('form.error.missing.email'),
+      },
+      email: {
+        message: intl.t('form.error.invalid.email'),
+      },
+    },
+    password: {
+      presence: {
+        allowEmpty: false,
+        message: intl.t('form.error.missing.password'),
+      },
+    },
+  };
+
   return (
     <KeyboardAvoidingView behavior="padding">
-      <IntlConsumer>
-        {(intl) => (
-          <>
-            <TextInput
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardType="email-address"
-              returnKeyType="next"
-              onChangeText={setEmail}
-              placeholder={intl.t('form.input.email')}
-            />
-            {errors.email && <Text>{errors.email[0]}</Text>}
-            <TextInput
-              returnKeyType="go"
-              onChangeText={setPassword}
-              placeholder={intl.t('form.input.password')}
-              secureTextEntry
-            />
-            {errors.password && <Text>{errors.password[0]}</Text>}
-            <Button
-              onPress={() => {
-                const constraints = {
-                  email: {
-                    presence: {
-                      allowEmpty: false,
-                      message: intl.t('form.error.missing.email'),
-                    },
-                    email: {
-                      message: intl.t('form.error.invalid.email'),
-                    },
-                  },
-                  password: {
-                    presence: {
-                      allowEmpty: false,
-                      message: intl.t('form.error.missing.password'),
-                    },
-                  },
-                };
-                const validatedErrors = validate({ email, password }, constraints);
-
-                setErrors(validatedErrors || {});
-                if (validatedErrors === undefined) {
-                  onSubmit(email, password);
-                }
-              }}
-              title={intl.t('form.submit.signUp')}
-            />
-          </>
-        )}
-      </IntlConsumer>
+      <FormInput
+        label="email"
+        keyboardType="email-address"
+        returnKeyType="next"
+        onChangeText={setEmail}
+      />
+      {errors.email && <Text>{errors.email[0]}</Text>}
+      <FormInput label="password" returnKeyType="go" onChangeText={setPassword} secureTextEntry />
+      {errors.password && <Text>{errors.password[0]}</Text>}
+      <FormButton
+        title="signUp"
+        onSubmit={() => onSubmit(email, password)}
+        constraints={{ fields: { email, password }, rules: constraints }}
+        onError={setErrors}
+        labelStyle={Styles.signInButtonLabel}
+      />
     </KeyboardAvoidingView>
   );
 };
