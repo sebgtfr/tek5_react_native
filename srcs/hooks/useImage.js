27434d4dd0a541f118e703a75673179f1a3fa8a3
soul_wander <valentin.mareschal@epitech.eu>
2021-01-30 19:13:16 +0300

    Take from camera

--- srcs/hooks/useImage.js
+++ srcs/hooks/useImage.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import * as ImagePicker from 'expo-image-picker';
+//import { result } from 'validate.js';
 
-const useImage = (type) => {
+const useImage = (type, methode) => {
   const [image, setImage] = React.useState(null);
-  const pickImage = React.useCallback(async () => {
-    if (Platform.OS !== 'web') {
-      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to make this work!');
-        return;
+  const pickImage = React.useCallback(
+    async (methode) => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+          return;
+        }
+        if (methode === false) {
+          const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            base64: true,
+            quality: 0.4,
+          });
+          if (!result.cancelled) {
+            setImage(type == 'uri' ? result.uri : result.base64);
+          }
+        } else {
+          const result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            base64: true,
+            quality: 0.4,
+          });
+          if (!result.cancelled) {
+            setImage(type == 'uri' ? result.uri : result.base64);
+          }
+        }
       }
-      const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        base64: true,
-        quality: 0,
-      });
-      if (!result.cancelled) {
-        setImage(type == 'uri' ? result.uri : result.base64);
-      }
-    }
-  }, [setImage]);
+    },
+    [setImage]
+  );
   return React.useMemo(() => ({ image, pickImage }), [image, pickImage]);
 };
 
--- srcs/screens/Profile/index.jsx
+++ srcs/screens/Profile/index.jsx
@@ -11,17 +11,59 @@ import Avatar from '../../components/Avatar';
 
 import Styles from './Styles';
 import avatar from '../../images/defaultAvatar.png';
+import { Button, Portal, Dialog, TextInput, RadioButton } from 'react-native-paper';
 
 const Profile = () => {
   const firebase = React.useContext(FirebaseContext);
-
   const { image, pickImage } = useImage('uri');
+  const [checked, setChecked] = React.useState(false);
+  const [isDialogVisible, setIsDialogVisible] = React.useState(false);
+
   React.useEffect(() => {
     if (image !== null) firebase.edit({ photoURL: image });
   }, [image]);
   return (
     <View style={Styles.container}>
-      <TouchableOpacity onPress={pickImage}>
+      <TouchableOpacity onPress={() => setIsDialogVisible(true)}>
+        <Portal>
+          <Dialog visible={isDialogVisible} onDismiss={() => setIsDialogVisible(false)}>
+            <Dialog.Title>Choose methode</Dialog.Title>
+            <Dialog.Content>
+              <Text>take picture from camera</Text>
+              <RadioButton
+                value="true"
+                status={checked === true ? 'checked' : 'unchecked'}
+                onPress={() => setChecked(true)}
+              />
+              <Text>take picture from gallery</Text>
+              <RadioButton
+                value="false"
+                status={checked === false ? 'checked' : 'unchecked'}
+                onPress={() => setChecked(false)}
+              />
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button
+                onPress={() => {
+                  setChecked(false);
+                  setIsDialogVisible(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                onPress={() => {
+                  setIsDialogVisible(false);
+                  pickImage(checked);
+
+                  setChecked(false);
+                }}
+              >
+                Submit
+              </Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
         <Avatar src={firebase.user.photoURL ? { uri: firebase.user.photoURL } : avatar} />
       </TouchableOpacity>
 
@@ -34,3 +76,7 @@ const Profile = () => {
 Profile.propTypes = {};
 
 export default Profile;
+
+/*<TouchableOpacity onPress={pickImage}>
+        <Avatar src={firebase.user.photoURL ? { uri: firebase.user.photoURL } : avatar} />
+      </TouchableOpacity>*/
