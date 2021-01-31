4551b91b9836a575783e871494a97c884132bc98
Sébastien Le Maire <sebastien.le-maire@epitech.eu>
2021-01-31 16:53:37 +0100

    add props listitem

--- srcs/components/forms/utils/FormList/index.jsx
+++ srcs/components/forms/utils/FormList/index.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Text, View, FlatList, Image } from 'react-native';
 
-const FormList = ({ items }) => {
-  return (
-    <>
-      <FlatList
-        data={items}
-        renderItem={({ item }) => (
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Image style={{ width: 128, height: 128 }} source={item.photoURL} />
-            <Text>
-              Name: {item.name}
-              {'\n'}Description: {item.desc}
-              {'\n'}By: {item.email}
-              {'\n'}
-            </Text>
-          </View>
-        )}
-        keyExtractor={(item) => item._id}
-      />
-    </>
-  );
+const FormList = ({ items }) => (
+  <FlatList
+    data={items}
+    renderItem={({ item: { name, desc, email, photoURL } }) => (
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Image style={{ width: 128, height: 128 }} source={photoURL} />
+        <Text>
+          Name: 
+{' '}
+{name}
+          {'\n'}
+          Description:
+          {desc}
+          {'\n'}
+          By:
+          {email}
+          {'\n'}
+        </Text>
+      </View>
+    )}
+    keyExtractor={({ id }) => id}
+  />
+);
+
+FormList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      desc: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      photoURL: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default FormList;
