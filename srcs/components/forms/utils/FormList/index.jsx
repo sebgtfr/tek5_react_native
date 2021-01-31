37f00428b109bbe4c79f984d16ac383f7230ea0b
soul_wander <valentin.mareschal@epitech.eu>
2021-01-31 19:29:09 +0300

    Add posibility to buy item

--- srcs/components/forms/utils/FormList/index.jsx
+++ srcs/components/forms/utils/FormList/index.jsx
@@ -3,61 +3,64 @@ import PropTypes from 'prop-types';
 
 import { Text, View, FlatList, Image } from 'react-native';
 import { Button, Dialog } from 'react-native-paper';
-import MyItems from '../../../../screens/MyItems';
 import { ButtonIntl } from '../../../../components/intl';
 import useCollection from '../../../../hooks/useCollection';
 
 const FormList = ({ items, myItems }) => {
   const [visible, setVisible] = React.useState(false);
+
   const show = React.useCallback(() => setVisible(true), [setVisible]);
   const hide = React.useCallback(() => setVisible(false), [setVisible]);
-  setToSolde = (item) => {
-    useCollection('items').doc(item.key).update({ solde: true });
+
+  setToSolde = (key) => {
+    useCollection('items').doc(key).update({ solde: true });
   };
+
   return (
-  <FlatList
-    data={items}
-    renderItem={({ item: { name, desc, email, photoURL } }) => (
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <TouchableOpacity onPress={show}>
-        <Image style={{ width: 128, height: 128 }} source={photoURL} />
-        <Text>
-          Name: 
-{' '}
-{name}
-          {'\n'}
-          Description:
-          {desc}
-          {'\n'}
-          By:
-          {email}
-          {'\n'}
-        </Text>
-            </TouchableOpacity>
-            <Portal>
-              <Dialog visible={visible} onDismiss={hide}>
-                <Dialog.Content style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                  <ButtonIntl
-                    uppercase
-                    title="button.cancel"
-                    onSubmit={() => {
-                      setToSolde(item);
-                      hide;
-                    }}
-                  />
-                  <ButtonIntl
-                    uppercase
-                    title={MyItems ? 'button.setSold' : 'Button.buy'}
-                    onSubmit={() => setSold(true)}
-                  />
-                </Dialog.Content>
-              </Dialog>
-            </Portal>
-      </View>
-    )}
-    keyExtractor={({ key }) => key}
-  />
-);
+    <FlatList
+      data={items}
+      renderItem={({ item: { name, desc, email, photoURL, key } }) => (
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <TouchableOpacity onPress={show}>
+            <Image style={{ width: 128, height: 128 }} source={photoURL} />
+            <Text>
+              Name: {name}
+              {'\n'}
+              Description:
+              {desc}
+              {'\n'}
+              By:
+              {email}
+              {'\n'}
+            </Text>
+          </TouchableOpacity>
+          <Portal>
+            <Dialog visible={visible} onDismiss={hide}>
+              <Dialog.Content style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <ButtonIntl
+                  uppercase
+                  title="button.cancel"
+                  onSubmit={() => {
+                    hide;
+                  }}
+                />
+                <ButtonIntl
+                  uppercase
+                  title={myItems ? 'button.setSold' : 'Button.buy'}
+                  onSubmit={() => {
+                    setToSolde(key);
+                    hide;
+                  }}
+                />
+              </Dialog.Content>
+            </Dialog>
+          </Portal>
+        </View>
+      )}
+      keyExtractor={({ key }) => key}
+    />
+  );
+};
 
 FormList.propTypes = {
   items: PropTypes.arrayOf(
@@ -70,6 +73,7 @@ FormList.propTypes = {
       key: PropTypes.string.isRequired,
     })
   ).isRequired,
+  myItems: PropTypes.bool.isRequired,
 };
 
 export default FormList;
